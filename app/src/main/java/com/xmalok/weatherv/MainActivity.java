fd507aedd2e9a231e8297aa65c04b3f21f234bf7
x-malok <x-malok>
2018-03-05 21:28:51 +0300

    Commit 1

--- app/src/main/java/com/xmalok/weatherv/MainActivity.java
+++ app/src/main/java/com/xmalok/weatherv/MainActivity.java
@@ -2,14 +2,16 @@ package com.xmalok.weatherv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String LOG_TAG= "mylog";
+  public   final String LOG_TAG= "mylog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    Log.d(LOG_TAG,"activity create");
     }
 }
